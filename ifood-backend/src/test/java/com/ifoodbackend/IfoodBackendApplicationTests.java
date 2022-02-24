2afe75bfb568bf9b99bf8bf001c20b7f46caacee
Murilo Badoco Zup <murilo.badoco@zup.com.br>
2022-02-24 16:25:08 -0300

    setup: projeto spring para o backend

---
+++ ifood-backend/src/test/java/com/ifoodbackend/IfoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
