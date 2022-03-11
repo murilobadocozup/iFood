3eff792cc4c135af3bf063f625eee9b6978fd51f
Murilo Badoco Zup <murilo.badoco@zup.com.br>
2022-03-11 10:21:24 -0300

    add: Tratamento de exceções

---
+++ ifood-backend/src/main/java/com/ifoodbackend/exceptions/FieldErrorOutputDto.java
@@ -0,0 +1,20 @@
+package com.ifoodbackend.exceptions;
+
+public class FieldErrorOutputDto {
+
+    private final String field;
+    private final String message;
+
+    public FieldErrorOutputDto(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
